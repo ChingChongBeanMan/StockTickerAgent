ebe91cb876c0c365062bb361d58d9552d05ef92a
jerry <jerry1bean@hotmail.com>
2016-02-19 15:45:08 -0800

    adjusted menu

--- application/views/mainmaster.php
+++ application/views/mainmaster.php
@@ -1,9 +1,9 @@
 <div id="content">
 	<div class="jumbotron text-center col-md-12" style="background-image: url(assets/mm.jpg); background-size: 100%;">
-		<div class ="col-xl-12 col-md-6">
+		<div class ="col-xl-12 col-md-12">
 			{mainview}
 		</div>
-		<div class ="col-xl-12 col-md-6">
+		<div class ="col-xl-12 col-md-12">
 			{sideview}
 		</div>
 	</div>
