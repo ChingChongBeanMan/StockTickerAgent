8c03b4a3b0c961e6eb994ab33399c5a4392e5b24
Colin Bose <colin.bose2@gmail.com>
2016-04-16 12:11:26 -0700

    Added login - had to make own db so might have to fix later

---
+++ application/models/Users.php
@@ -0,0 +1,31 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class Users extends MY_Model {
+    // constructor
+    function __construct() {
+        parent::__construct('users','username');
+    }
+ function addUser($username,$password,$role){
+        $this->load->database();
+        
+        $data = array(
+            'username'      => $username ,
+            'password'      => $password ,
+            'role'          => $role ,
+        );
+        $this->db->insert('users', $data); 
+    }
+    function getUser(){
+        $this->load->database();
+        $this->db->select('*');
+        $this->db->from('users');
+        $qet = $this->db->get();
+        return $qet;
+    }
+}
