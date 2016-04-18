b4943be136fe361319cecdcbfe2c492826df92ab
Colin Bose <colin.bose2@gmail.com>
2016-04-17 17:39:10 -0700

    small fix to buy

--- application/views/BuySell.php
+++ application/views/BuySell.php
@@ -12,8 +12,10 @@
                 <td>{name}</td>
                 <td>{code}</td>
                 <td>{value}</td>
+                <td><a href="/StockManager/buyStocks/{code}/10/{username}">Buy 10</a></td>
                 <td><a href="/StockManager/buyStocks/{code}/50/{username}">Buy 50</a></td>
                 <td><a href="/StockManager/buyStocks/{code}/50/{username}">Buy 100</a></td>
+                <td><a href="/StockManager/sellStocks/{code}/10/{username}">Sell 10</a></td>
                 <td><a href="/StockManager/sellStocks/{code}/50/{username}">Sell 50</a></td>
                 <td><a href="/StockManager/sellStocks/{code}/100/{username}">Sell 100</a></td>
             </tr>
